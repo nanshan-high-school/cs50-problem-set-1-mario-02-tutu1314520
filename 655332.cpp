0bcbc7db8405da72c26ebbe4e81ed8d3cd7894a6
小杜 <57487152+tutu1314520@users.noreply.github.com>
2019-11-23 18:10:12 +0800

    Create 655332.cpp

---
+++ 655332.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int times;
+    do {
+        cout << "高度:";
+        cin >> times;
+       } while(times < 1 || times > 8);
+
+       for(int t = 0; t < times;t++) {
+           for(int j=1;j<=5-t;j++){
+		   cout << " ";
+		}
+		for(int n=0;n<=t;n++){
+		   cout << "#";
+		}
+          cout << "  ";
+           for (int n = 0; n <= t;n++){
+                cout << "#";
+           }
+           cout << "\n";
+       }      
+}// 272 32 杜宸緯 655332
+
